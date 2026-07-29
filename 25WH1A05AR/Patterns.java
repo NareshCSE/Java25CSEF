d1f907ce9846561137e9df312f06a9a213807f53
laxmi sahasra <25wh1a05ar@bvrithyderabad.edu.in>
2026-07-29 16:23:02 +0530

    Add Patterns.java for printing patterns
    Implement a pattern printing program in Patterns.java

---
+++ 25WH1A05AR/Patterns.java
@@ -0,0 +1,26 @@
+package mypackage5ar;
+
+public class Patterns {
+
+	public Patterns() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int a=0;a<=5;a++) {
+			for(int b=a;b<=5;b++) {
+				System.out.print(" ");
+			}
+			for(int r=a;r>=0;r--) {
+			System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
+
+	
+
+
