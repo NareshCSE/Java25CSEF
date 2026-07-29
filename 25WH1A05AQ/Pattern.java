8bfa5cf2d634dc724ad1abe2b546e74267b9dbfe
JAMA SOWMYA <25wh1a05aq@bvrithyderabad.edu.in>
2026-07-29 16:22:03 +0530

    Create Pattern.java

---
+++ 25WH1A05AQ/Pattern.java
@@ -0,0 +1,22 @@
+package mypackage5aq;
+
+public class Patterns {
+
+	public Patterns() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<=5;i++) {
+			for(int j=0;j<=i;j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+			
+		}
+		
+
+	}
+
+}
