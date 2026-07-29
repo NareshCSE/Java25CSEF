706821cc14a438e8938ade2b0b857f21764d3ea2
25WH1A05AT <25wh1a05at@bvrithyderabad.edu.in>
2026-07-29 16:21:28 +0530

    Create Calculator.java

---
+++ 25WH1A05AT/Calculator.java
@@ -0,0 +1,75 @@
+package mypackage5at;
+
+import java.util.Scanner;
+
+
+public class Cal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			Scanner input=new Scanner(System.in);
+
+			System.out.println("Enter a");
+
+			int FirstValue=input.nextInt();
+
+			System.out.println("the first number is "+FirstValue);
+
+			System.out.println("Enter b");
+
+			int SecondValue=input.nextInt();
+
+			System.out.println("the second number is "+SecondValue);
+
+			System.out.println("enter operations : + - * / %");
+
+			char Operation=input.next().charAt(0);
+
+			switch (Operation)
+
+			{
+
+			case '+':
+
+				System.out.println("the sum is "+(FirstValue+SecondValue));
+
+				break;
+
+			case '-':
+
+				System.out.println("the diff is "+(FirstValue-SecondValue));
+
+				break;
+
+			case '*':
+				System.out.println("the prod is "+(FirstValue*SecondValue));
+
+				break;
+
+			case '/':
+
+				System.out.println("the quo is"+(FirstValue/SecondValue));
+
+				break;
+
+			case '%':
+
+				System.out.println("the reminder is "+(FirstValue%SecondValue));	
+
+				break;
+
+			default:
+
+	            System.out.println("Invalid operation");
+
+	    }
+
+
+
+	    input.close();			
+
+			}
+
+		}		
+
