f4fe9f1ffd4b0d212c89dd4d9cc82b53bd2b4c17
sravya-sras-9 <25wh1a05z2@bvrithyderabad.edu.in>
2026-07-29 16:21:23 +0530

    Add Pattern class to print asterisks triangle
    Implement a pattern printing program that prints a right-angled triangle of asterisks.

---
+++ 25WH1A05Z2/Pattern.java
@@ -0,0 +1,18 @@
+package mypackage5z2;
+
+public class Pattern {
+
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method 
+		int i,j;
+		for(i=1;i<=7;i++) {
+		for(j=1;j<=i;j++) {
+            System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+}
