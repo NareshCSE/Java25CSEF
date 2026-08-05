0fc5a9eba3874907cf9e2e04e327f28b2926457a
laxmi sahasra <25wh1a05ar@bvrithyderabad.edu.in>
2026-08-05 16:14:31 +0530

    Add Patterns5c class for printing star patterns

---
+++ 25WH1A05AR/Patterns b.java
@@ -0,0 +1,33 @@
+package mypackage5ar;
+
+public class Patterns5c {
+
+	public Patterns5c() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	for(int x=0;x<=5;x++) {
+		for(int y=0;y<=5-x;y++) {
+			System.out.print(" ");
+		}
+		for(int z=0;z<=x;z++) {
+			System.out.print("* ");
+		}
+		System.out.println();
+	}
+	for(int x=1;x<=5;x++) {
+		for(int y=0;y<=x;y++) {
+			System.out.print(" ");
+		}
+	for(int z=0;z<=5-x;z++) {
+		System.out.print("* ");
+		}
+		System.out.println();
+	}
+	}
+}
+
+
