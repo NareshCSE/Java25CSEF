82c76558d046c04e8a42e615af91199b982ef055
srivyshnaviii <25wh1a05cc@bvrithyderabad.edu.in>
2026-08-05 16:10:55 +0530

    Add Pattern1.java to print asterisks in a triangle

---
+++ 25WH1A05CC/Pattern1.java
@@ -0,0 +1,18 @@
+package mypackage5cc;
+
+public class Pattern {
+
+	public static void main(String[] args) {
+		int rows=7;
+		for(int i=0;i<rows;i++)
+		{
+			for(int j=0;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
