eababc726aab2e83a76161d2450226a10c901bb2
manasvisingh456 <25wh1a05z6@bvrithyderabad.edu.in>
2026-08-05 16:12:46 +0530

    Add diamond pattern printing in pattern1.java
    This program prompts the user for the number of rows and prints a diamond pattern using asterisks.

---
+++ 25WH1A05Z6/pattern1.java
@@ -0,0 +1,43 @@
+package mypackage5z6;
+
+import com.sun.tools.javac.parser.Scanner;
+
+public class Pattern3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter the number of rows for the top half: ");
+        int rows = sc.nextInt();
+        
+        
+        for (int i = 1; i <= rows; i++) {
+          
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+            
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            
+            System.out.println();
+        }
+        
+       
+        for (int i = rows - 1; i >= 1; i--) {
+            
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+          
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+           
+            System.out.println();
+        }
+        
+        sc.close();
+    }
+}
+
