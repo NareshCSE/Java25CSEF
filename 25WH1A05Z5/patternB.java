8c7ff17d392ef15b6ef401fbb15839aaa7edcac7
jasmitha4713 <25wh1a05z5@bvrithyderabad.edu.in>
2026-08-05 15:48:34 +0530

    Create patternB.java

---
+++ 25WH1A05Z5/patternB.java
@@ -0,0 +1,35 @@
+ackage myproject5z5;
+
+
+
+public class Pattern2 {
+
+
+
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stub
+
+		int i,j,k;
+
+		for(i=1;i<=7;i++) {
+
+			for(j=1;j<=7-i;j++) {
+
+				System.out.print(" ");
+
+			}
+
+			for(k=1;k<=i;k++) {
+
+			System.out.print("*");
+
+		}
+
+			System.out.println();
+
+		}
+
+  }
+
+	}
