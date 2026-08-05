880821badd5c36ac98e6b3ec5800222c7cea50af
kalidindi-harshitha <25wh1a05z7@bvrithyderabad.edu.in>
2026-08-05 16:17:00 +0530

    Add Pattern3 class for printing a pattern

---
+++ 25WH1A05Z7/Pattern3.java
@@ -0,0 +1,27 @@
+package mypackage5z7;
+
+public class Pattern3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			for(int i=1;i<7;i++) {
+
+		    	  for(int j=1;j<i;j++) {
+
+		    		  System.out.print(" ");
+
+		    	  }
+
+		    	  for(char k='A';k<'H'-i;k++) {
+
+		    		  System.out.print(k);
+
+		    	  }
+
+		    	  System.out.println();
+
+		      }
+
+			}
+		
+}
