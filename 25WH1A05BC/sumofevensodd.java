334001b1db2830f71c3fba62f5473f4a4d8a7a79
Hasini godugu <25wh1a05bc@bvrithyderabad.edu.in>
2026-08-05 15:32:52 +0530

    Add files via upload

---
+++ 25WH1A05BC/sumofevensodd.java
@@ -0,0 +1,34 @@
+package myproject5bc;
+
+import java.util.Scanner;
+
+public class sumofevensodd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("enter number of integers:");
+		Scanner input=new Scanner(System.in);
+		int n=input.nextInt();
+		
+		System.out.println("enter"+ n +"integers:");
+		int evensum=0;
+		int oddsum=0;
+		for(int i=0;i<=n;i++) {
+			int num=input.nextInt();
+			
+			if(num%2==0) {
+				evensum+=num;
+			}
+			else {
+				oddsum+=num;
+			}
+		}
+		System.out.println("Even sum:"+evensum);
+		System.out.println("Odd sum:"+oddsum);
+		
+		
+		
+
+	}
+
+}
