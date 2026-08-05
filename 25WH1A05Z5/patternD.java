bc81a6d74d552631835b7144b30bc85c2d67efbf
jasmitha4713 <25wh1a05z5@bvrithyderabad.edu.in>
2026-08-05 15:43:49 +0530

    Create patternD

---
+++ 25WH1A05Z5/patternD.java
@@ -0,0 +1,33 @@
+package z5;
+
+
+
+public class pattern5 {
+
+
+
+	public static void main(String[] args) {
+
+      for(int i=1;i<7;i++) {
+
+    	  for(int j=1;j<i;j++) {
+
+    		  System.out.print(" ");
+
+    	  }
+
+    	  for(char k='A';k<'H'-i;k++) {
+
+    		  System.out.print(k);
+
+    	  }
+
+    	  System.out.println();
+
+      }
+
+	}
+
+
+
+}
