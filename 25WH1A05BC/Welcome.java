a1c5a7abbbe296e38399742c14ce49875dc50fe1
Hasini godugu <25wh1a05bc@bvrithyderabad.edu.in>
2026-08-05 15:34:47 +0530

    Add files via upload

---
+++ 25WH1A05BC/Welcome.java
@@ -0,0 +1,16 @@
+package myproject5bc;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Java");
+		int number1=100;
+		int number2=200;
+		int sum=number1+number2;
+		System.out.println(sum);
+		System.out.println("I am hasini");
+		System.out.println("the result is "+sum);
+	}
+
+}
