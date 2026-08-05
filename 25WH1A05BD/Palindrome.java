0dcc857f968d659217d39ba0762cb68f6b3423db
25wh1a05bd-del <25wh1a05bd@bvrithyderabad.edu.in>
2026-08-05 16:14:01 +0530

    Create Palindrome

---
+++ 25WH1A05BD/Palindrome.java
@@ -0,0 +1,28 @@
+package myproject5bd;
+
+import java.util.Scanner;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input=new Scanner(System.in);
+		System.out.println("enter a string:");
+		String str=input.next();
+		int l=str.length();
+		int c=0;
+		for(int i=0;i<l/2;i++) {
+			if(str.charAt(i) !=str.charAt(l-i-1)){
+				System.out.println("not a palindrome");
+				break;
+			}
+			c++;
+		}
+		if(c==l/2) {
+		System.out.println("the string is a palindrome");
+		}
+	}
+}
+
+
+
