e285df79297b380eabfe32d68300cbb174c6e494
Medhasvi13 <25wh1a05z3@bvrithyderabad.edu.in>
2026-08-12 15:45:27 +0530

    Add Welcome.java with basic Java program

---
+++ 25WH1A05Z3/Welcome.java
@@ -0,0 +1,15 @@
+package mypackage5z3;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Welcome to java");
+        int number1=400;
+        int number2=200;
+        int sum=number1-number2;
+        System.out.println("the result is"+sum);
+        
+	}
+
+}
