a945c644293709d3106e4cc77def4df674ee5e90
pranathi0718 <25wh1a05bp@bvrithyderabad.edu.in>
2026-08-12 16:01:00 +0530

    Create pattern4.java

---
+++ 25WH1A05BP/pattern4.java
@@ -0,0 +1,55 @@
+package mypackage5bp;
+
+public class pattern3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+				int n = 7;
+
+		        int i,j,k;
+
+		        for (i = 1; i <= n; i++) {
+
+		            for (j = 1; j <= n - i; j++) {
+
+		                System.out.print(" ");
+
+		            }
+
+		            for (k = 1; k<=i; k++) {
+
+		                System.out.print(" *");
+
+		            }
+
+		            System.out.println();
+
+		        }
+
+		        for(i=n-1;i>=1;i--) {
+
+		        	for(j=1;j<=n-i;j++) {
+
+		        		System.out.print(" ");
+
+		        	}
+
+		        	for (k = 1; k <= i; k++) {
+
+		                System.out.print(" *");
+
+		            }
+
+		            System.out.println();
+
+		        }
+
+
+
+			}
+
+
+
+}
+
