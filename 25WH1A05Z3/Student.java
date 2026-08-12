52d734e38b42c4b543a0e1d01be7949289d24138
Medhasvi13 <25wh1a05z3@bvrithyderabad.edu.in>
2026-08-12 15:43:15 +0530

    Add Student class with properties and methods

---
+++ 25WH1A05Z3/Student.java
@@ -0,0 +1,27 @@
+package mypackage5z3;
+
+public class Student   {//props = variables
+	String rollnumber;
+	String address;
+	String email;
+	int mobile_number;
+	double cgpa;
+	
+	//behaviours = methods
+	double getCGPA(String rollnumber) {
+		return cgpa;
+	}
+	
+	double getAttendance() {
+		return cgpa;
+	}
+	public static void main(String[] args) {
+		Student medha;
+		medha= new Student(); //
+		System.out.println(medha);
+		
+	
+
+	}
+
+}
