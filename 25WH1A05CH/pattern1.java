b7bbea134f90e3a766b02d148b80cc1b0118f12c
25wh1a05ch-RP <25wh1a05ch@bvrithyderabad.edu.in>
2026-08-12 15:54:51 +0530

    Add files via upload

---
+++ 25WH1A05CH/pattern1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package mypackage5ch;
+
+public class pattern1 {
+	public static void main(String[] args) {
+		int n=5;
+		for(int i=0;i<n;i++)
+		{
+			for(int j=i;j<n;j++)
+			{
+				System.out.print("@");
+			}
+			System.out.println();
+		}
+		// TODO Auto-generated method stub
+	}
+
+}
