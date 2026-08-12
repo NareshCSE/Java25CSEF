28213511f4a6267e2aa3330a960001f044409bee
25wh1a05be <25wh1a05be@bvrithyderabad.edu.in>
2026-08-12 15:36:06 +0530

    Pattern5
    Implement a pattern printing program that displays a pyramid of numbers.

---
+++ 25WH1A05BE/Pattern5.java
@@ -0,0 +1,24 @@
+package mypackage5be;
+
+public class pattern5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=7;
+		int i,j;
+		for(i=1;i<=n;i++) {
+			for(j=1;j<=i;j++) {
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+		for(i=n-1;i>=1;i--) {
+			for(j=1;j<=i;j++) {
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
