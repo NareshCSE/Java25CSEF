cd8c1d76421f2931078966c3259ab10e5c8ac734
sravya-sras-9 <25wh1a05z2@bvrithyderabad.edu.in>
2026-08-12 15:40:59 +0530

    Create classDemo with employee details and methods
    Added classDemo with employee properties and methods.

---
+++ 25WH1A05Z2/classDemo.java
@@ -0,0 +1,30 @@
+package mypackage5z2;
+
+public class classDemo {
+	//properties/variables
+	int employee_id;
+	String address;
+	String email;
+	int mobile_no;
+	double emp_salary;
+	//Behavior=methods
+	double getEmployeeSalary(String emp_id/{
+		return emp_salary;
+		
+	}
+	String getEmpAddress(int emp_id)
+	{
+		return address;
+	}
+	Employee[] getEmployees(String dept_name) {
+		return null;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+Employee naresh = new Employee();
+
+System.out.println(naresh);
+	}
+
+}
