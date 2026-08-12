0dbb44d345ed1042d01f1d7f2c2756abe649453b
25wh1a05bd-del <25wh1a05bd@bvrithyderabad.edu.in>
2026-08-12 15:45:11 +0530

    Create Switch.java

---
+++ 25WH1A05BD/Switch.java
@@ -0,0 +1,70 @@
+package myproject5bd;
+import java.util.Scanner;
+
+public class Switch {
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stub
+
+		System.out.println("Enter First number:");
+
+		Scanner input=new Scanner(System.in);
+
+		int number1=input.nextInt();
+
+		System.out.println("Enter Second number:");
+
+        int number2=input.nextInt();
+
+		System.out.println("Enter the operator:");
+
+		char operator=input.next().charAt(0);
+
+		switch(operator) {
+
+		case'+':
+
+		System.out.println("Sum of "+number1+" and "+number2+" is:"+(number1+number2));
+
+		break;
+
+		
+
+		case'-':
+
+		System.out.println("Difference of "+number1+" and "+number2+" is:"+(number1-number2));
+
+		break;
+
+		
+
+		case'*':
+
+		System.out.println("Multiplication of "+number1+" and "+number2+" is:"+(number1*number2));
+
+		break;
+
+		
+
+		case'/':
+
+		System.out.println("division of "+number1+" and "+number2+"is:"+(number1/number2));
+
+		break;
+
+		
+
+			}
+
+
+
+	}
+
+
+
+}
+
+
+       
+    	   
+
