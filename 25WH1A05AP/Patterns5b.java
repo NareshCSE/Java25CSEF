9ac492a240d46b3febb503b7c0635d16af00fbcb
snahini-allari <25wh1a05ap@bvrithyderabad.edu.in>
2026-08-12 16:13:12 +0530

    Add files via upload

---
+++ 25WH1A05AP/Patterns5b.java
@@ -0,0 +1,20 @@
+package mypackage5ap;
+
+public class Patterns5b {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int p=0;p<=5;p++) {
+			for(int q=p;q<=5;q++) {
+				System.out.print(" ");
+			}
+			for(int r=p;r>=0;r--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+
+	}
+
+}
