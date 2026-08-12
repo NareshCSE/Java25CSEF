7f7c69de6c521e823dd08ed16f360cca4fdf3b9a
Vaishnavi-Baiti <25wh1a05cb@bvrithyderabad.edu.in>
2026-08-12 16:16:28 +0530

    Create student.java

---
+++ 25WH1A05CB/student.java
@@ -0,0 +1,27 @@
+package myproject5cb;
+
+public class Student {
+	
+		String rollNumber;
+		String address;
+		String email;
+		int mobile_number;
+		double cgpa;
+
+		double getCGPA(String rollnumber) {
+			return cgpa;
+			
+			
+		}
+		double getAttendence() {
+			return cgpa;
+		}
+		public static void main(String[] args) {
+			Student vaishnavi=new Student();
+			System.out.println(vaishnavi);
+			
+
+	}
+
+}
+		
