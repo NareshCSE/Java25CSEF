34b64130dde41b9177c8110eb36f0ed1b6f3c510
Medhasvi13 <25wh1a05z3@bvrithyderabad.edu.in>
2026-08-12 15:36:13 +0530

    Add Pattern5 class to print asterisks in a pattern

---
+++ 25WH1A05Z3/Pattern5.java
@@ -0,0 +1,30 @@
+package mypackage5z3;
+
+public class Pattern5 {
+
+		public static void main(String[] args) {
+
+			// TODO Auto-generated method stub
+
+			int i,j;
+
+			for(i=1;i<=7;i++) {
+
+				for(j=1;j<=i;j++) {
+
+					System.out.print("*");
+
+				}
+
+			System.out.println();
+
+			}
+
+
+
+		}
+
+
+
+	}
+
