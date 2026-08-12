50ee980cb61571fd2b087beb8f59fe3e77122eaa
vaishnavireddy287 <25wh1a05bl@bvrithyderabad.edu.in>
2026-08-12 16:16:01 +0530

    Add files via upload

---
+++ 25WH1A05BL/palindrome.java
@@ -0,0 +1,25 @@
+package myproject5bl;
+
+import java.util.Scanner;
+
+public class palindrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input=new Scanner(System.in);
+		System.out.println("enter a string:");
+		String str=input.next();
+		int l=str.length();
+		int c=0;
+		for(int i=0;i<l/2;i++) {
+			if(str.charAt(i) !=str.charAt(l-i-1)){
+				System.out.println("not a palindrome");
+				break;
+			}
+			c++;
+		}
+		if(c==l/2) {
+		System.out.println("the string is a palindrome");
+	}
+
+}}
