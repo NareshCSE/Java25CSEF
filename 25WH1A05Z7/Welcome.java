f179d13a3ebf2c03c21ed11b1ee7160610c2ae73
kalidindi-harshitha <25wh1a05z7@bvrithyderabad.edu.in>
2026-08-12 14:05:17 +0530

    Add Welcome class to display a welcome message

---
+++ 25WH1A05Z7/Welcome.java
@@ -0,0 +1,16 @@
+package mypackage5z7;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Java");
+		int number1=100; 
+		int number2=200;
+		int sum=number1+number2;
+		System.out.println("the result is"+sum);
+		
+
+	}
+
+}
