80f2c651b1bf47c4c8fca403ee6f2eeb4e0c498d
25wh1a05bd-del <25wh1a05bd@bvrithyderabad.edu.in>
2026-08-12 15:40:15 +0530

    Create Duplicates.java

---
+++ 25WH1A05BD/Duplicates.java
@@ -0,0 +1,27 @@
+package myproject5bd;
+
+import java.util.Scanner;
+
+public class Duplicates {
+ 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input=new Scanner(System.in);
+		System.out.println("enter size of array:");
+		int n=input.nextInt();
+		int[] arr=new int[n];
+		System.out.println("enter array elements:");
+		for(int i=0;i<n;i++) {
+			arr[i]=input.nextInt();
+		}
+		System.out.println("duplicates elements:");
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<i;j++) {
+				if(arr[i]==arr[j]) {
+					System.out.println(arr[i]);
+					break;
+				}
+			}
+		}
+	}
+}
