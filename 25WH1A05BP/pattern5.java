ae39786dec417afce0224d170689d7b47ad50522
pranathi0718 <25wh1a05bp@bvrithyderabad.edu.in>
2026-08-12 16:16:23 +0530

    Add pattern5.java to print number patterns

---
+++ 25WH1A05BP/pattern5.java
@@ -0,0 +1,35 @@
+package mypackage5bp;
+
+public class pattern5 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stud
+			int n=7;
+
+			int i,j;
+
+			for(i=1;i<=n;i++) {
+
+				for(j=1;j<=i;j++) {
+
+					System.out.print(j+" ");
+
+				}
+
+				System.out.println();
+
+			}
+
+			for(i=n-1;i>=1;i--) {
+
+				for(j=1;j<=i;j++) {
+
+					System.out.print(j+" ");
+
+				}
+
+				System.out.println();
+
+			}
+
+ 		}
+}
