91e42a5c5f57aaac1d521b53b8426af0e1a540d8
Nivarshika-9 <25wh1a05bk@bvrithyderabad.edu.in>
2026-08-12 16:10:35 +0530

    Add files via upload

---
+++ 25WH1A05BK/patternA.java
@@ -0,0 +1,20 @@
+package myproject5bk;
+
+import java.util.Scanner;
+
+public class patternA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input=new Scanner(System.in);
+		System.out.println("enter the number rows");
+		int row=input.nextInt();
+		for(int i=0;i<=row;i++) {
+			for(int j=0;j<=i;j++) {
+				System.out.print(" * ");
+			}
+		System.out.println();
+	}}
+	
+
+}
