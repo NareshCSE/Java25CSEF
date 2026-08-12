25d4e081ddac526d62bcf929b580bbb63ea5385f
Medhasvi13 <25wh1a05z3@bvrithyderabad.edu.in>
2026-08-12 15:46:21 +0530

    Add Employee class with basic properties and methods
    Created Employee class with properties and methods to retrieve salary and address.

---
+++ 25WH1A05Z3/Employee.java
@@ -0,0 +1,37 @@
+package mypackage5z3;
+
+public class Employee {
+
+
+   //props = variables
+	int emp_id;
+	String emp_name;
+	String emp_salary;
+	String address;
+	int mobile_number;
+	double cgpa;
+	
+	//behaviours = methods
+	String getEmployeeSalary (int emp_id) {
+		return emp_salary;
+	}
+	
+	
+	String getEmpAddress(int emp_id) {
+		return address;
+	}
+	
+	Employee[] getEmployees(String dept_name) {
+		return null;
+	}
+	
+	
+	public static void main(String[] args) {
+		Employee medha = new Employee();
+		System.out.println(medha);
+		
+	
+
+	}
+
+}
