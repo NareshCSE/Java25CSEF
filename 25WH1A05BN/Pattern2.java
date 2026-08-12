437b35abd336b4da323c0eb2fe7c641dd88c3b1d
25WH1A05BN <25wh1a05bn@bvrithyderabad.edu.in>
2026-08-12 16:20:35 +0530

    Create Pattern2.java

---
+++ 25WH1A05BN/Pattern2.java
@@ -0,0 +1,23 @@
+package myproject;
+
+public class Pattern2 {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			int n=7;
+			for (int i=1;i<=n;i++) 
+			{
+				for (int j=1;j<=n;j++) 
+				{
+					System.out.print(" ");
+				}
+				for (int k=1;k<=i;k++) 
+				{
+					System.out.print("* ");
+				}
+				System.out.println();
+			} 
+		}
+
+	}
+
