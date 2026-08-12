404500c9eb3e20ee89d0b02a5f17d3dda9ba2c17
Medhasvi13 <25wh1a05z3@bvrithyderabad.edu.in>
2026-08-12 15:35:29 +0530

    Add Pattern4 class to print a star pattern
    Implement a pattern printing program in Java.

---
+++ 25WH1A05Z3/Pattern4.java
@@ -0,0 +1,37 @@
+package mypackage5z3;
+
+public class Pattern4 {
+
+
+
+
+
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stub
+
+				int i,j,k;
+
+				for(i=1;i<=7;i++) {
+
+					for(j=1;j<=7-i;j++) {
+
+						System.out.print(" ");
+
+					}
+
+					for(k=1;k<=i;k++) {
+
+						System.out.print("*");
+
+					}
+
+					System.out.println();
+
+	} 
+
+
+
+}
+
+}
