f6c81d5522d051b53ba33a0545dc81264ee08467
25WH1A05BN <25wh1a05bn@bvrithyderabad.edu.in>
2026-08-12 15:53:48 +0530

    Add Student class with attributes and methods

---
+++ 25WH1A05BN/Student.java
@@ -0,0 +1,22 @@
+package myproject;
+
+public class Student {
+	String rollnumber;
+	String address;
+	String email;
+	int mobilenumber;
+	double cgpa;
+	double getCGPA(String rollnumber) {
+		return cgpa;
+	}
+	double getAttendance() {
+		return cgpa;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student veda = new Student();
+		veda = new Student();
+		System.out.println(veda);
+	}
+
+}
