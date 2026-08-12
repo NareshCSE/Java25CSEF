15f4aa3856cb653f720cbd38ba223a87d06568e5
25wh1a05aw-Rashika <25wh1a05aw@bvrithyderabad.edu.in>
2026-08-12 15:56:40 +0530

    Create pattern1.java

---
+++ 25WH1A05AW/pattern1.java
@@ -0,0 +1,18 @@
+package mypackage5aw;
+
+public class pattern1 {
+
+	public static void main(String[] args) {
+		for(int i=0;i<7;i++) 
+		{
+		    for(int j=0;j<i+1;j++)
+		    {
+		    	System.out.print("* ");
+		    }
+		    System.out.println(" ");
+		}
+		
+
+	}
+
+}
