d117b275c09c67c64d3bf94dcd57cb414802346d
Anusha-Chittiraju123 <25wh1a05cd@bvrithyderabad.edu.in>
2026-08-19 15:30:09 +0530

    Create right_pyramid2.java

---
+++ 25WH1A05CD/right_pyramid2.java
@@ -0,0 +1,18 @@
+package mypackage5cd;
+
+public class Pattern_2 {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=7;i++) {
+			for(int j=1;j<=7-i;j++) {
+				System.out.print(" ");
+			}
+			for(int k=1;k<=i;k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
