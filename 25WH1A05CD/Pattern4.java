06d45436a72cae3ced421294849a9e637df75099
Anusha-Chittiraju123 <25wh1a05cd@bvrithyderabad.edu.in>
2026-08-19 15:25:00 +0530

    Create Pattern4.java

---
+++ 25WH1A05CD/Pattern4.java
@@ -0,0 +1,16 @@
+package mypackage5cd;
+
+public class Pattern_4 {
+
+	public static void main(String[] args) {
+		String alp="ABCDEFG";
+		for(int i=1;i<7;i++) {
+			for(int j=0;j<=i;j++) {
+				System.out.println(alp );
+			}
+			System.out.println();
+		}
+
+	}
+
+}
