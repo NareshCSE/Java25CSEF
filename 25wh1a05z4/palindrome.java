b10a6174e32046458eaeb78b8ef35f485d6ea58f
brahmani-09 <25wh1a05z4@bvrithyderabad.edu.in>
2026-09-09 01:20:54 +0530

    Create palindrome.java

---
+++ 25wh1a05z4/palindrome.java
@@ -0,0 +1,22 @@
+package myproject;
+
+import java.util.Scanner;
+
+public class Palindrome {
+	    public static void main(String[] args) {
+	    	Scanner input=new Scanner(System.in);
+
+	    	        System.out.print("Enter String: ");
+	    	        String str = input.nextLine();
+	    	        String rev = "";
+
+	    	        for(int i = str.length()-1; i >= 0; i--)
+	    	            rev += str.charAt(i);
+
+	    	        if(str.equals(rev))
+	    	            System.out.println("Palindrome");
+	    	        else
+	    	            System.out.println("Not Palindrome");
+	    	    }
+	    	}
+	    
