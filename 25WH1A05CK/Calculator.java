e5c9f05eba28c2f3932828785aac048cc4b8bf8b
Akshitha94127 <25wh1a05ck@bvrithyderabad.edu.in>
2026-09-13 10:59:39 +0530

    Implement basic calculator with user input
    Added a basic calculator functionality to perform addition, subtraction, multiplication, and division based on user input.

--- 25WH1A05CK/Calculator.java
+++ 25WH1A05CK/Calculator.java
@@ -1,1 +1,31 @@
+package mypackage5ck;
 
+import java.util.Scanner;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input=new Scanner(System.in);
+		System.out.println("Enter first number:");
+		int number1=input.nextInt();
+		System.out.println("Enter second number");
+	    int number2=input.nextInt();
+		int operator=input.nextInt();
+		switch (operator) {
+		case 1:
+			System.out.println("Addition"+(number1+number2));
+			break;
+		case 2:
+			System.out.println("Substraction"+(number1-number2));
+			break;
+		case 3:
+			System.out.println("Multiplication"+(number1*number2));
+			break;
+		case 4:
+			System.out.println("Division"+(number1/number2));
+			break;
+			
+		}
+		}
+	}
