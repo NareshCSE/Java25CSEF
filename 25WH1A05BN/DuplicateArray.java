dea9e838bcb664299bb42b16fc5e62dbac40d009
25WH1A05BN <25wh1a05bn@bvrithyderabad.edu.in>
2026-09-16 14:16:34 +0530

    Add DuplicateArray class to find duplicate values

---
+++ 25WH1A05BN/DuplicateArray.java
@@ -0,0 +1,22 @@
+package myproject;
+
+public class DuplicateArray {
+
+    public static void main(String[] args) {
+
+        int[] a = {10, 20, 30, 20, 40, 10, 50};
+
+        System.out.println("Duplicate values:");
+
+        for (int i = 0; i < a.length; i++) {
+
+            for (int j = i + 1; j < a.length; j++) {
+
+                if (a[i] == a[j]) {
+                    System.out.println(a[i]);
+                    break;
+                }
+            }
+        }
+    }
+}
