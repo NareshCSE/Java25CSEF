e90618e9560fbb73c55aa7abedf82ec85267f298
25WH1A05AT <25wh1a05at@bvrithyderabad.edu.in>
2026-09-23 16:04:09 +0530

    Add Loop5C.java to print diamond pattern
    Implement a program that prints a diamond pattern based on user input for the number of rows.

---
+++ 25WH1A05AT/Loop 5C.java
@@ -0,0 +1,35 @@
+package mypackage5at;
+
+import java.util.Scanner;
+
+public class Loop5c {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("enter number of rows:");
+		int rows=scanner.nextInt();
+		for(int i=1;i<=rows;i++) {
+			for (int j=1;j<=rows-i;j++) {
+				System.out.print(" ");
+			}
+				for (int k=1;k<=i;k++) {
+				
+				   System.out.print("* ");
+			}
+			System.out.println();
+		}
+		for(int i=rows-1;i>=1;i--) {
+			for(int j=1; j<=rows-i; j++) {
+				System.out.print(" ");
+			}
+			for (int k=1;k<=i;k++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+			
+		}
+
+	}
+
+}
